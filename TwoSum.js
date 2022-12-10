b38b1384bf6105842e0951262da0e57b6c2158ce
vl-kazakov <kazakov-vol@yandex.ru>
2022-12-10 20:42:19 +0300

    Тест обновление

--- TwoSum.js
+++ TwoSum.js
@@ -3,11 +3,9 @@ let target = 9;
 var twoSum = function (nums, target) {
 	for (i = 0; i < nums.length; i++) {
 		for (j = i + 1; j < nums.length; j++) {
-
 			if (i !== j) {
 				if (nums[i] + nums[j] === target) {
 					return [i, j]
-
 				}
 			}
 		}
